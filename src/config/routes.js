74491082a3cb3b41f1609520e1f5f9c83ec163f8
achenjs <hellowwc@aliyun.com>
2018-06-04 23:51:30 +0800

    重构路由

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,57 +1,94 @@
-const routes = [
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import StatisticsSetting from '../pages/StatisticsSetting';             //  统计设置页
+import OrderManage from '../pages/OrderManage';                         //  订单管理页
+import BuyData from '../pages/BuyData';                                 //  购买数据页
+import FinancialDataMaintain from '../pages/FinancialDataMaintain';     //  财务数据管理
+import NotFound from '../pages/NotFound';                               //  404
+
+import Details from '../pages/OrderManage/Details';
+import Statistics from '../pages/OrderManage/Statistics';
+
+import Account from '../pages/BuyData/Account';
+import Client from '../pages/BuyData/Client';
+import Renewal from '../pages/BuyData/Renewal';
+
+export const routes = [
     {
         path: '/statisticsSetting',
         name: '统计设置',
         icon: 'area-chart',
-        children: [{
-            path: '/statisticsSetting/filterList',
-            name: '过滤清单',
-        }]
-    },
-    {
+        component: StatisticsSetting,
+        children: [
+            {
+                path: '/statisticsSetting/filterList',
+                name: '过滤清单'
+            }
+        ]
+    }, {
         path: '/orderManage',
         name: '订单管理',
-        children: [{
-            path: '/orderManage/details',
-            name: '订单详情',
-        }, {
-            path: '/orderManage/statistics',
-            name: '订单统计',
-        }]
-    },
-    {
+        component: OrderManage,
+        children: [
+            {
+                path: '/orderManage/details',
+                name: '订单详情',
+                component: Details
+            }, {
+                path: '/orderManage/statistics',
+                name: '订单统计',
+                component: Statistics
+            }
+        ]
+    }, {
         path: '/buyData',
         name: '购买数据',
-        children: [{
-            path: '/buyData/account',
-            name: '账号统计',
-        }, {
-            path: '/buyData/client',
-            name: '客户统计',
-        }, {
-            path: '/buyData/renewal',
-            name: '续签统计',
-        }]
-    },
-    {
+        component: BuyData,
+        children: [
+            {
+                path: '/buyData/account',
+                name: '账号统计',
+                component: Account
+            }, {
+                path: '/buyData/client',
+                name: '客户统计',
+                component: Client
+            }, {
+                path: '/buyData/renewal',
+                name: '续签统计',
+                component: Renewal
+            }
+        ]
+    }, {
         path: '/financialDataMantain',
         name: '财务数据管理',
-        children: [{
-            path: '/financialDataMantain/importData',
-            name: '导入数据',
-        }, {
-            path: '/financialDataMantain/price',
-            name: '价格表',
-        }]
-    },
-    {
+        component: FinancialDataMaintain,
+        children: [
+            {
+                path: '/financialDataMantain/importData',
+                name: '导入数据'
+            }, {
+                path: '/financialDataMantain/price',
+                name: '价格表'
+            }
+        ]
+    }, {
         path: '/saas',
-        name: 'SaaS数据指标',
-    },
-    {
+        name: 'SaaS数据指标'
+    }, {
         path: '/logs',
-        name: '操作日志',
+        name: '操作日志'
+    }, {
+        path: '*',
+        component: NotFound
     }
 ];
 
-export default routes;
+export const RouteWithSubRoutes = function (route) {
+    return (
+        <Route
+            path={route.path}
+            render={props => (<route.component {...props} routes={route.routes}/>)}/>
+    );
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import App from '../App';
-import StatisticsSetting from '../pages/StatisticsSetting';             //  统计设置页
-import OrderManage from '../pages/OrderManage';                         //  订单管理页
-import BuyData from '../pages/BuyData';                                 //  购买数据页
-import FinancialDataMaintain from '../pages/FinancialDataMaintain';     //  财务数据管理
-import NotFound from '../pages/NotFound';                               //  404
 
-import Details from '../pages/OrderManage/Details';
-import Statistics from '../pages/OrderManage/Statistics';
+import { RouteWithSubRoutes, routes } from '@/config/routes';
 
-import Account from '../pages/BuyData/Account';
-import Client from '../pages/BuyData/Client';
-import Renewal from '../pages/BuyData/Renewal';
 class RouteMap extends Component {
     render() {
         return (
@@ -20,23 +11,9 @@ class RouteMap extends Component {
                 <Route component={props => (
                     <App {...props}>
                         <Switch>
-                            <Route path="/" exact component={StatisticsSetting} />
-                            <Route path="/statisticsSetting" component={StatisticsSetting} />
-                            <Route path="/orderManage" children={() => (
-                                <OrderManage>
-                                    <Route path="/orderManage/details" component={Details} replace />
-                                    <Route path="/orderManage/statistics" component={Statistics} replace />
-                                </OrderManage>
-                            )} />
-                            <Route path="/buyData" render={() => (
-                                <BuyData>
-                                    <Route path="/buyData/account" component={Account} replace />
-                                    <Route path="/buyData/client" component={Client} replace />
-                                    <Route path="/buyData/renewal" component={Renewal} replace />
-                                </BuyData>
-                            )} />
-                            <Route path="/financialDataMantain" component={FinancialDataMaintain} />
-                            <Route component={NotFound} />
+                             {routes.map((route, i) => (
+                                <RouteWithSubRoutes key={i} {...route} />
+                            ))}
                         </Switch>
                     </App>
                 )} />
